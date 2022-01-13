e5b6aeb44c0f19a5023f010a242ebb9cc4d3b16d
Christopher Ng <chrng8@gmail.com>
2022-01-13 02:22:29 +0000

    Scaffold pages for admin and personal settings
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

---
+++ lib/Controller/PageController.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright 2022 Christopher Ng <chrng8@gmail.com>
+ *
+ * @author Christopher Ng <chrng8@gmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\UserMigration\Controller;
+
+use OCA\UserMigration\AppInfo\Application;
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IRequest;
+use OCP\Util;
+
+class PageController extends Controller {
+
+	public function __construct(IRequest $request) {
+		parent::__construct(Application::APP_ID, $request);
+	}
+
+	/**
+	 * @NoAdminRequired
+	 * @NoSubAdminRequired
+	 */
+	public function index() {
+		Util::addScript(Application::APP_ID, 'user_migration-main');
+		return new TemplateResponse(Application::APP_ID, 'main');
+	}
+}
---
+++ lib/Settings/Personal/Section.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright 2022 Christopher Ng <chrng8@gmail.com>
+ *
+ * @author Christopher Ng <chrng8@gmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\UserMigration\Settings\Personal;
+
+use OCA\UserMigration\AppInfo\Application;
+use OCP\IL10N;
+use OCP\IURLGenerator;
+use OCP\Settings\IIconSection;
+
+class Section implements IIconSection {
+
+	/** @var IURLGenerator */
+	private $url;
+	/** @var IL10N */
+	private $l;
+
+	public function __construct(
+		IURLGenerator $url,
+		IL10N $l
+	) {
+		$this->url = $url;
+		$this->l = $l;
+	}
+
+	public function getIcon(): string {
+		return $this->url->imagePath(Application::APP_ID, 'app-light.svg');
+	}
+
+	public function getID(): string {
+		return Application::APP_ID;
+	}
+
+	public function getName(): string {
+		return $this->l->t(Application::APP_NAME);
+	}
+
+	public function getPriority(): int {
+		return 80;
+	}
+}

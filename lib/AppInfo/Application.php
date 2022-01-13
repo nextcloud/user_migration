e3c471b9454ab6c1be3cd2d318b6f7efad8057f7
Christopher Ng <chrng8@gmail.com>
2022-01-13 20:29:23 +0000

    Cleanup - Add authors - Add licenses
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -1,11 +1,36 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * @copyright 2022 Christopher Ng <chrng8@gmail.com>
+ *
+ * @author Christopher Ng <chrng8@gmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 namespace OCA\UserMigration\AppInfo;
 
 use OCP\AppFramework\App;
 
 class Application extends App {
 	public const APP_ID = 'user_migration';
+	public const APP_NAME = 'User Migration';
 
 	public function __construct() {
 		parent::__construct(self::APP_ID);

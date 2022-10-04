5140691d50bed3ec2d60d8a59539bb7148ae58d1
Christopher Ng <chrng8@gmail.com>
2022-10-04 17:58:49 +0000

    Remove extraneous admin settings code
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
 webpackConfig.entry = {
-	'admin-settings': path.join(__dirname, 'src', 'admin-settings.js'),
 	'personal-settings': path.join(__dirname, 'src', 'personal-settings.js'),
 }
 

ae1eca339575cdcc5f8bdff4c44912ead4cfa9a5
Côme Chilliet <91878298+come-nc@users.noreply.github.com>
2025-12-02 17:36:57 +0100

    chore: Fix license in lib/Command/Manage.php
    Co-authored-by: Kate <26026535+provokateurin@users.noreply.github.com>
    Signed-off-by: Côme Chilliet <91878298+come-nc@users.noreply.github.com>

--- lib/Command/Manage.php
+++ lib/Command/Manage.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 /**
- * SPDX-FileCopyrightText: 2022 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: AGPL-3.0-only
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 namespace OCA\UserMigration\Command;
